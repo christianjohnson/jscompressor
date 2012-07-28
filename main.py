a3779b3a9b3b6f87efd5396997c0698dc30950f8
Christian Johnson <christian@christianjohnson.org>
2012-07-27 22:28:17 -0700

    JS Compressor Finalized

--- main.py
+++ main.py
@@ -1,44 +1,39 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# Copyright Christian Johnson 2012
+
 import cgi
 import hashlib
 import webapp2
 import urllib
+import logging
 from google.appengine.api import memcache
 from google.appengine.api import urlfetch
 
-COMPRESSIONS = ['WHITESPACE_ONLY', 'SIMPLE_OPTIMIZATIONS', 
-                'ADVANCED_OPTIMIZATIONS']
-
+COMPRESSIONS = ['WHITESPACE_ONLY', 'SIMPLE_OPTIMIZATIONS', 'ADVANCED_OPTIMIZATIONS']
 URL = 'http://closure-compiler.appspot.com/compile'
 
-
-def check_memcache(key):
+def check_memcache(key, compilation_level):
   md5_key = hashlib.md5(key).hexdigest()
-  return memcache.get(md5_key)
-
+  return memcache.get(md5_key + ':' + compilation_level)
 
 class MainHandler(webapp2.RequestHandler):
-
+  """Redirect to GitHub."""
+  
   def get(self):
+    self.redirect("http://christianjohnson.github.com/jscompressor/")
+
+class Compress(webapp2.RequestHandler):
+  """"Class to handle compression requests.
+  
+  Args:
+    RequestHandler: WebApp RequestHandler
+  """
+  
+  def get(self):
+    self.response.headers['Content-Type'] = 'text/plain'
     cache_time = urllib.unquote(cgi.escape(self.request.get(
         'cache_time', '3600')))
-    compilation_level = urllib.unquote(cgi.escape(
-        self.request.get('compilation_level', COMPRESSIONS[0])))
+    compilation_level = str(urllib.unquote(cgi.escape(
+        self.request.get('compilation_level', COMPRESSIONS[0]))))
     js_code = urllib.unquote(cgi.escape(self.request.get('js_code', '')))
     code_url = urllib.unquote(cgi.escape(self.request.get('code_url', '')))
     
@@ -50,12 +45,11 @@ class MainHandler(webapp2.RequestHandler):
     elif code_url:
       key = code_url
     
-    data = check_memcache(key)
+    data = check_memcache(key, compilation_level)
+    
     if data is not None:
-      self.response.headers['Content-Type'] = 'text/plain'
       self.response.out.write(data)
       return
-    
     
     params = urllib.urlencode([
         ('code_url', code_url),
@@ -65,14 +59,20 @@ class MainHandler(webapp2.RequestHandler):
         ('output_info', 'compiled_code'),
     ])
     
-    headers = { 'Content-type': 'application/x-www-form-urlencoded' }
+    headers = {'Content-type': 'application/x-www-form-urlencoded'}
     result = urlfetch.fetch(url=URL, payload=params, method=urlfetch.POST,
                             headers=headers)
     
     if result.status_code == 200:
-      memcache.add(hashlib.md5(key).hexdigest(), result.content, 
-          int(cache_time))
-      self.response.headers['Content-Type'] = 'text/plain'
-      self.response.out.write(result.content)
+      try:
+        memcache.add(hashlib.md5(key).hexdigest() + ':' + compilation_level, 
+                     result.content, 
+                     int(cache_time))
+        self.response.out.write(result.content)
+      except ValueError:
+        self.response.out.write("Error: Cache time not a valid number.")
+    else:
+      self.response.out.write("Error: Please try again soon.")
 
-app = webapp2.WSGIApplication([('/', MainHandler)], debug=True)
+app = webapp2.WSGIApplication([('/compress', Compress),
+                               ('/', MainHandler)], debug=True)
